64351b6b14657460acc7713e0e1fef44b8520720
Avinash <akgpcamp@gmail.com>
2024-09-20 13:16:31 +0530

    modified add favorite service

--- src/main/java/in/kodder/controller/AdminFoodController.java
+++ src/main/java/in/kodder/controller/AdminFoodController.java
@@ -4,16 +4,14 @@ import in.kodder.entity.Food;
 import in.kodder.entity.Restaurant;
 import in.kodder.entity.User;
 import in.kodder.request.CreateFoodRequest;
+import in.kodder.response.MessageResponse;
 import in.kodder.service.FoodService;
 import in.kodder.service.RestaurantService;
 import in.kodder.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/admin/food")
@@ -28,11 +26,30 @@ public class AdminFoodController {
     @Autowired
     private RestaurantService restaurantService;
 
+    @PostMapping
     private ResponseEntity<Food> createFood(@RequestBody CreateFoodRequest req,
                                             @RequestHeader("Authorization") String token) throws Exception {
         User user = userService.findUserByJwtToken(token);
         Restaurant restaurant = restaurantService.findRestaurantById(req.getRestaurantId());
-        Food food = foodService.createFood(req,req.getCategory(),restaurant);
+        Food food = foodService.createFood(req, req.getCategory(), restaurant);
         return new ResponseEntity<>(food, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<MessageResponse> deleteFood(@PathVariable Long id,
+                                                       @RequestHeader("Authorization") String token) throws Exception {
+        User user = userService.findUserByJwtToken(token);
+        foodService.deleteFood(id);
+        MessageResponse message = new MessageResponse();
+        message.setMessage("Successfully deleted food");
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<Food> updateFoodAvailabilityStatus(@PathVariable Long id,
+                                                              @RequestHeader("Authorization") String token) throws Exception {
+        User user = userService.findUserByJwtToken(token);
+        Food food = foodService.updateAvailabilityStatus(id);
+        return new ResponseEntity<>(food, HttpStatus.OK);
     }
 }
--- src/main/java/in/kodder/controller/FoodController.java
+++ src/main/java/in/kodder/controller/FoodController.java
@@ -1,9 +1,47 @@
 package in.kodder.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import in.kodder.entity.Food;
+import in.kodder.entity.User;
+import in.kodder.service.FoodService;
+import in.kodder.service.RestaurantService;
+import in.kodder.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/food")
 public class FoodController {
+
+    @Autowired
+    private FoodService foodService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @GetMapping("/search")
+    private ResponseEntity<List<Food>> searchFood(@RequestParam String name,
+                                                  @RequestHeader("Authorization") String token) throws Exception {
+        User user = userService.findUserByJwtToken(token);
+        List<Food> food = foodService.searchFood(name);
+        return new ResponseEntity<>(food, HttpStatus.OK);
+    }
+
+    @GetMapping("/restaurant/{restaurantId}")
+    private ResponseEntity<List<Food>> getRestaurantFood(@RequestParam boolean vegeterian,
+                                                         @RequestParam boolean nonvegeterian,
+                                                         @RequestParam boolean seasonal,
+                                                         @RequestParam(required = false) String foodCategory,
+                                                         @PathVariable Long restaurantId,
+                                                         @RequestHeader("Authorization") String token) throws Exception {
+        User user = userService.findUserByJwtToken(token);
+        List<Food> food = foodService.getRestaurantFoods(restaurantId, vegeterian, nonvegeterian, seasonal, foodCategory);
+        return new ResponseEntity<>(food, HttpStatus.OK);
+    }
 }
